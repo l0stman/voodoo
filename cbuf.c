4056564fcfe0fa70c83af8efe30ea769e5ce06dd
l0stman <endian.sign@gmail.com>
2015-12-30 10:53:49 +0300

    Replace the two calls to read_or_die in cread by a single call to readv_or_die.
    Likewise replace the two calls to write_or_die in cwrite by a single
    call to writev_or_die.  And the value returned should be the one
    returned by the latter since for a non-blocking socket, the number of
    bytes being written could be less than "nbytes".

--- cbuf.c
+++ cbuf.c
@@ -38,8 +38,12 @@ cread(int fd, struct cbuf *cbp, size_t nbytes)
         assert(nbytes <= CBUFSIZ - cbp->len);
         offset = (cbp->offset + cbp->len) & CBUFMASK;
         if (offset + nbytes > CBUFSIZ) {
-                nread = read_or_die(fd, cbp->buf + offset, CBUFSIZ - offset);
-                nread += read_or_die(fd, cbp->buf, offset + nbytes - CBUFSIZ);
+                struct iovec v[2];
+                v[0].iov_base = cbp->buf + offset;
+                v[0].iov_len = CBUFSIZ - offset;
+                v[1].iov_base = cbp->buf;
+                v[1].iov_len = offset + nbytes - CBUFSIZ;
+                nread = readv_or_die(fd, v, 2);
         } else
                 nread = read_or_die(fd, cbp->buf + offset, nbytes);
         cbp->len += nread;
@@ -49,14 +53,20 @@ cread(int fd, struct cbuf *cbp, size_t nbytes)
 ssize_t
 cwrite(int fd, struct cbuf *cbp, size_t nbytes)
 {
+        ssize_t nwrite;
+
         if (cbp->len < nbytes)
                 err_quit("cwrite: not enough bytes in the buffer");
         if (cbp->offset + nbytes > CBUFSIZ) {
-                write_or_die(fd, cbp->buf + cbp->offset, CBUFSIZ - cbp->offset);
-                write_or_die(fd, cbp->buf, cbp->offset + nbytes - CBUFSIZ);
+                struct iovec v[2];
+                v[0].iov_base = cbp->buf + cbp->offset;
+                v[0].iov_len = CBUFSIZ - cbp->offset;
+                v[1].iov_base = cbp->buf;
+                v[1].iov_len = cbp->offset + nbytes - CBUFSIZ;
+                nwrite = writev_or_die(fd, v, 2);
         } else
-                write_or_die(fd, cbp->buf + cbp->offset, nbytes);
-        cbp->len -= nbytes;
-        cbp->offset = (cbp->offset + nbytes) & CBUFMASK;
-        return (nbytes);
+                nwrite = write_or_die(fd, cbp->buf + cbp->offset, nbytes);
+        cbp->len -= nwrite;
+        cbp->offset = (cbp->offset + nwrite) & CBUFMASK;
+        return (nwrite);
 }
